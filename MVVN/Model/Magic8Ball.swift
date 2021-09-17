36915b7c087dca867d3ef3f6046b4942877f0b47
Mo Asche <masche@lcs.on.ca>
2021-09-17 11:56:35 -0400

    added a model file to separate responses data from our files

---
+++ MVVN/Model/Magic8Ball.swift
@@ -0,0 +1,43 @@
+//
+//  MAGIC 8 BALL.swift
+//  MVVN
+//
+//  Created by Moritz Conrad Asche on 2021-09-17.
+//
+
+import Foundation
+
+struct Magic8Ball {
+    
+    func getResponse() -> String {
+        let responses = [
+            // Affirmative answers
+            "It is certain",
+            "It is decidely so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            
+            // Non-committal answers
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not to tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            
+            // Negative answers
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+        ]
+        return responses.randomElement()!
+        
+    }
+}
--- MVVN/main.swift
+++ MVVN/main.swift
@@ -1,11 +1,32 @@
 //
 //  main.swift
-//  MVVN
+//  Magic8Ball
 //
-//  Created by Moritz Conrad Asche on 2021-09-17.
+//  Created by Russell Gordon on 2021-09-16.
 //
 
 import Foundation
 
-print("Hello, World!")
+// Program name
+print("MAGIC 8 BALL")
+print("============")
 
+// Ask for user's question
+print("""
+NOTE: Questions should be phrased such
+      that they can be answered with a
+      yes or no response.
+
+""")
+print("What is your question? ")
+// Get input as a non-optional String
+let input = readLine()!
+
+// Print out their question
+print("")
+print("You said: \(input)")
+print("")
+
+
+let advisor = Magic8Ball()
+print(advisor.getResponse())
